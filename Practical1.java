df8f4383356a41b025a5027b3baa64c767a2e1af
Orion-Odd <77224492+Orion-Odd@users.noreply.github.com>
2021-07-08 12:26:39 +0530

    Add files via upload
    Design a class Complex having a real part (x) and an imaginary part (y). Provide methods to
    perform the following on complex numbers:
    1. Add two complex numbers.
    2. Multiply two complex numbers.
    3. toString() method to display complex numbers in the form: x + i y

---
+++ Practical1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Complex
+{
+    private int x, y;    //real - imaginary
+    Scanner sc = new Scanner(System.in);
+
+    Complex(int real, int imaginary){
+        x = real;
+        y = imaginary;
+    }
+
+    public static void addTwoComplex(Complex c1, Complex c2){
+        int r,i;
+        r = c1.x + c2.x;
+        i = c1.y + c2.y;
+        System.out.println("Final complex number: "+r+" + "+i+"i");
+    }
+
+    public static void multiplyTwoComplex(Complex c1, Complex c2){
+        int r, i;              //(x+yi)(u+vi) = (xu - yv) + (xv + yu)i       -> Rule for multiplication
+        r = c1.x*c2.x - c1.y*c2.y;
+        i = c1.x*c2.y + c1.y*c2.x;
+        System.out.println("Final Complex number: "+r+" + "+i+"i");
+    }
+
+    @Override
+    public String toString(){
+        return x+ " +i"+y; 
+    }
+}
+
+public class Practical1
+{
+    public static void main(String[] args) {
+        Complex c1 = new Complex(3,2);
+        Complex c2 = new Complex(1,4);
+        Complex.addTwoComplex(c1,c2);
+        Complex.multiplyTwoComplex(c1, c2);
+        System.out.println(c1.toString());
+    }
+}
